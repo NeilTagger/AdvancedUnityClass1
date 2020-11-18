9a7ff4beaef21288704c00540bd5758d97d27c76
neiliam <neil.tagger@gmail.com>
2020-11-18 20:30:29 +0200

    Finished paddles, ball and game logic, only missing goal logic.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player 1")]
+    public GameObject Player1Paddle;
+    public GameObject Player1Goal;
+
+    [Header("Player 2")]
+    public GameObject Player2Paddle;
+    public GameObject Player2Goal;
+
+    [Header("ScoreUI")]
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+
+    private int Player1Score;
+    private int Player2Score;
+
+    public void Player1Scored()
+    {
+        Player1Score++;
+        Player1Text.GetComponent<TextMeshPro>().text = Player1Score.ToString();
+        ResetPosition();
+    }
+
+    public void Player2Scored()
+    {
+        Player2Score++;
+        Player2Text.GetComponent<TextMeshPro>().text = Player2Score.ToString();
+        ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        Player1Paddle.GetComponent<Paddle>().Reset();
+        Player2Paddle.GetComponent<Paddle>().Reset();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
